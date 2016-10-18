28266c4eccacfb0b4d706f154974b0c21bb43b64
Kevin Murray <spam@kdmurray.id.au>
2016-10-18 17:00:22 +1100

    Add dcs_size_min/max

--- src/dcs_util.h
+++ src/dcs_util.h
@@ -3,4 +3,10 @@
 
 #define dcs_free(ptr) ({if(ptr) free(ptr); ptr=NULL;})
 
+inline size_t dcs_size_min(size_t a, size_t b)
+{ return a < b ? a : b; }
+
+inline size_t dcs_size_max(size_t a, size_t b)
+{ return a > b ? a : b; }
+
 #endif /* end of include guard: DCS_UTIL_H_VO1MI98D */
